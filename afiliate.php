93db34684232e851153336b1b16ea6b674cf2e66
yoel <yt_entertainment@hotmail.com>
2016-06-24 13:56:25 -0400

    Le agregue una pagina para registrarse
    Una pagina donde puedo crear usuarios

--- afiliate.php
+++ afiliate.php
@@ -8,26 +8,43 @@
 <h1>Registrate aqui:</h1>
 
 
-<form id="form1" name="form1" method="post" action="entrada.php">
+<form id="form1" name="form1" method="post" action="registrardatos.php">
 <table width="350" height="98" border="1">
          <tr>
-      <td height="23" colspan="2" bgcolor="#66FF00"> <div align="center"><strong>LOGIN</strong></div></td>
+      <td height="23" colspan="2" bgcolor="#58ACFA"> <div align="center"><strong>Escribe tus datos</strong></div></td>
     </tr>
     <tr>
-     <td width="89" height="20" bgcolor="#66FF00"><div align="center"><strong>Usuario:</strong></div></td>
-      <td width="197"><input name="login" type="text" id="login" value="<?php  echo $login; ?>" required /></td>
+     <td width="89" height="20" bgcolor="#58ACFA"><div align="center"><strong>Nombre:</strong></div></td>
+      <td width="197"><input name="nombre" type="text" id="nombre" required /></td>
     </tr>
     <tr>
-     <td height="26" bgcolor="#66FF00"><div align="center"><strong>Contraseña:</strong></div></td>
+     <td height="26" bgcolor="#58ACFA"><div align="center"><strong>Dirección:</strong></div></td>
+      <td><input type="text" name="direccion" id="direccion" required/></td>
+    </tr>
+       <tr>
+     <td width="89" height="20" bgcolor="#58ACFA"><div align="center"><strong>Sexo:</strong></div></td>
+      <td width="197"><input name="sexo" type="text" id="sexo"  required /></td>
+    </tr>
+    <tr>
+     <td height="26" bgcolor="#58ACFA"><div align="center"><strong>Correo:</strong></div></td>
+      <td><input type="text" name="correo" id="correo"/></td>
+    </tr>
+     <tr>
+     <td width="89" height="20" bgcolor="#58ACFA"><div align="center"><strong>Codigo:</strong></div></td>
+      <td width="197"><input name="codusu" type="text" id="codusu"  required /></td>
+    </tr>
+       <tr>
+     <td width="89" height="20" bgcolor="#58ACFA"><div align="center"><strong>Usuario:</strong></div></td>
+      <td width="197"><input name="login" type="text" id="login"  required /></td>
+    </tr>
+    <tr>
+     <td height="26" bgcolor="#58ACFA"><div align="center"><strong>Contraseña:</strong></div></td>
       <td><input type="password" name="clave" id="clave" required/></td>
     </tr>
     <tr>
       <td colspan="2" align="center" valign="middle"><p>
-        <input type="submit" name="Entrar" id="Entrar" value="Entrar" />
-      </p>
-        <p><font color="#FF0000" size="2" face="Arial, Helvetica, sans-serif">
-          <?php if ($login == 1){echo "USUARIO NO EXISTE"; }  ?>
-        </font> </p></td>
+        <input type="submit" name="registrar" id="registrar" value="Registrar" />
+     
     </tr>
           </table>
           
